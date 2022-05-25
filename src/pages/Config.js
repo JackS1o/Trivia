93d529e80e59aef85090fbbf9a108dafb2cd0814
Aline Soliman <alineapreis@hotmail.com>
2022-05-25 17:48:46 -0300

    adicio 18 incompleto

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Config extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <span data-testid="settings-title">Configurações</span>
       </div>
     );
