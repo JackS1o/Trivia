07a0e1e96edd2fe57354227b5c1bab9887ee7f47
kaioruan <kaio.ruan@souunit.com.br>
2022-05-26 19:12:45 -0300

    req 09 - 11 - 12

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,10 @@
 export const USER_LOGIN = 'USER_LOGIN';
 export const USER_TOKEN = 'USER_TOKEN';
 export const USER_SCORE = 'USER_SCORE';
+export const USER_ASSERTIONS = 'USER_ASSERTIONS';
 export const ERROR_TOKEN = 'ERROR_TOKEN';
 export const TRUE_TIMER = 'TRUE_TIMER';
+export const VALUE_TIMER = 'VALUE_TIMER';
 
 export const startPlay = (start) => ({
   type: USER_LOGIN,
@@ -18,9 +20,18 @@ export const setScore = (score) => ({
   type: USER_SCORE,
   payload: score,
 });
+export const setAssertions = (assertions) => ({
+  type: USER_ASSERTIONS,
+  payload: assertions,
+});
 export const setTimer = (bool) => ({
   type: TRUE_TIMER,
   payload: bool,
+});
+
+export const quantifyTimer = (time) => ({
+  type: VALUE_TIMER,
+  payload: time,
 });
 
 export const fetchToken = () => async () => {
